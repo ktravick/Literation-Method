4757cbb2fd8c7497ee8cfc67ad3b4205772eae88
koko11 <ktravick30@gmail.com>
2020-07-10 16:22:25 -0400

    project

---
+++ main.js
@@ -0,0 +1,15 @@
+const sampleArray = [
+
+]
+
+const logitem = function (item) {
+
+    console.log("item:", item)
+
+
+}
+
+const result1 = sampleArray.foreach(logitem)
+const result1 = sampleArray.foreach(logitem)
+
+
